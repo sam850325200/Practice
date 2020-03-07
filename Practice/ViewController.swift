8869538cf1cfe760afc23ff0f61cd9e3e2a35d3c
司宇軒 <sam850325200@gmail.com>
2020-03-07 14:34:10 +0800

    Initial Commit

---
+++ Practice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Practice
+//
+//  Created by 司宇軒 on 2020/3/7.
+//  Copyright © 2020 fubonlife. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
